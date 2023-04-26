d5ea542ef538a4200c4e3ee8f23c0e1a0a1284c9
Vats <hiravats@gmail.com>
2023-04-26 20:36:00 +0530

    Added few responsibilities

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,15 +1,17 @@
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcryptjs')
+const crypto = require('crypto')
+const jexl = require('jexl')
 const asyncHandler = require('express-async-handler')
 const User = require('../models/userModel')
+const UKey = require('../models/userKeyModel')
 
 // @desc    Register new user
 // @route   POST /api/users
 // @access  Public
 const registerUser = asyncHandler(async (req, res) => {
 
-    const { name, email, password } = req.body
-    var attributes = []
+    const { name, email, password } = req.body    
 
     if(!name || !email || !password) {
         res.status(400)
@@ -102,11 +104,60 @@ const addAttributes = asyncHandler(async (req, res) => {
 
     updateAttributes.forEach( (attribute) => {
         tempAttr = [...tempAttr, attribute]
-    })
+    })  
 
     User.updateOne({_id : person.id}, {attributes : tempAttr})
         .then( () => console.log(' Attributes added '))
         .catch( (err) => console.error(' Error adding attributes ', err))
+    
+    try {
+        const { publicKey, privateKey } = await crypto.generateKeyPairSync('ec', {
+            namedCurve : 'secp256k1',
+            publicKeyEncoding : {
+                type : 'spki',
+                format : 'pem'
+            },
+            privateKeyEncoding : {
+                type : 'pkcs8',
+                format : 'pem'
+            }
+        })
+       
+        const updatedPublicKey = publicKey.replace(/-----BEGIN PUBLIC KEY-----\n/,'').replace(/-----END PUBLIC KEY-----\n/,'')
+        const updatedPrivateKey = privateKey.replace(/-----BEGIN PRIVATE KEY-----\n/,'').replace(/-----END PRIVATE KEY-----\n/,'')
+        
+        console.log(' Public key : \n',updatedPublicKey)
+        console.log('\n\n Private key : \n',updatedPrivateKey)  
+        
+        const userKeyExists = await UKey.findById(person.id)
+        let assignKey
+
+        if(userKeyExists) {
+            assignKey = await UKey.findByIdAndUpdate(person.id,{
+                publicKey : updatedPublicKey,
+                privateKey : updatedPrivateKey
+            })            
+        }
+        else {
+            assignKey = await UKey.create({
+                _id : person.id,
+                publicKey : updatedPublicKey,
+                privateKey : updatedPrivateKey
+            })
+        }        
+
+        if(assignKey) {
+            res.status(201)
+        }
+        else {
+            res.status(401)
+            throw new Error(' Could not update keys ')
+        }
+           
+        } catch (error) {
+            res.status(400)
+            throw new Error(' Could not add keys ')
+        }
 
     res.json({message : 'Attributes successfully added '})
 })
@@ -118,10 +169,38 @@ const generateToken = (id) => {
     })
 }
 
+// @desc    Access Policy
+// @route   POST /api/users/policy
+// @access  Private
+const generateAccessPolicy = asyncHandler( async (req, res) => {
+    const accessPolicy = "(CEO == true) || (CFO == true)"
+    const accessCondition = ['CEO','CFO']
+
+    const tempUser = {}
+
+    for(let i = 0; i < accessCondition.length; i++) {
+        const attr = accessCondition[i]
+        tempUser[attr] = true
+    }
+    
+    jexl.eval(accessPolicy, tempUser).then( (result) => {
+        if(result === true) {
+            console.log('user satisfies access policy')
+        }
+        else {
+            console.log(' not accepting access condition ')
+        }
+    })
+
+    res.json({message : 'Access Policy generated'})
+
+})
+
 
 module.exports = {
     registerUser,
     loginUser,
     getMe,
-    addAttributes,
+    addAttributes,   
+    generateAccessPolicy 
 }
