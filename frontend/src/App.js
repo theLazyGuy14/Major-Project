df57208c8fc2bd530c6af42602629447667c1419
Vats <hiravats@gmail.com>
2023-04-30 21:27:39 +0530

    Few changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,31 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route, useLocation} from 'react-router-dom'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css'
 import Header from './components/Header';
 import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import OwnerDashboard from './pages/OwnerDashboard'
+import NotePage from './pages/NotePage';
+import Attributes from './pages/Attributes';
 
 function App() {
   return (
     <>
-      <Router>
-        <div className = "container">
-          <Header />
+      <Router>        
+        <div className = "container">  
+          <Header />                  
           <Routes>
-            <Route path = '/' element = {<Dashboard />} />
+            <Route path = '/' element = {<Dashboard />} />  
+            <Route path = '/note' element = {<NotePage />}/>  
+            <Route path = '/home' element = {<OwnerDashboard />}/>            
             <Route path = '/login' element = {<Login />} />
-            <Route path = '/register' element = {<Register />} />
+            <Route path = '/register' element = {<Register />} /> 
+            <Route path = '/add' element = {<Attributes />}/>           
           </Routes>
         </div>
       </Router>
-      <ToastContainer />
+      <ToastContainer limit={1}/>
     </>
     
   );
